ee442ce2b7a81354d334eeeee34bb9a5e772dba4
BONGEUNGU <rnqhsrms98@gmail.com>
2025-08-29 15:50:52 +0900

    add conftest.py & import fixture

---
+++ test/conftest.py
@@ -0,0 +1,13 @@
+# Decoration Function 집합
+
+import pytest
+
+from apps.calculator import Calculator
+
+# 픽스쳐(fixture)
+@pytest.fixture
+def calculator_instance():
+    cal = Calculator()
+    return cal
+# Decoration Function
+# 테스트 함수에 전달 목적
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,38 +1,31 @@
 import pytest
 
 from apps.calculator import Calculator
+from test.conftest import calculator_instance
 
-# 픽스쳐(fixture)
-@pytest.fixture
-def calcurator_instance():
-    cal = Calculator()
-    return cal
-# Decoration Function
-# 테스트 함수에 전달 목적
-
-def test_add(calcurator_instance):
+def test_add(calculator_instance):
         
-    assert calcurator_instance.add(3,4) == 7
-    assert calcurator_instance.add(-1,1) == 0
-    assert calcurator_instance.add(-1, -98) == -99
+    assert calculator_instance.add(3,4) == 7
+    assert calculator_instance.add(-1,1) == 0
+    assert calculator_instance.add(-1, -98) == -99
     
-def test_sub(calcurator_instance):
+def test_sub(calculator_instance):
     
-    assert calcurator_instance.sub(5,2) == 3
-    assert calcurator_instance.sub(10, -2) == 12
-    assert calcurator_instance.sub(-2, 3) == -5
+    assert calculator_instance.sub(5,2) == 3
+    assert calculator_instance.sub(10, -2) == 12
+    assert calculator_instance.sub(-2, 3) == -5
     
-def test_div(calcurator_instance):
+def test_div(calculator_instance):
     
-    assert calcurator_instance.div(5, 3) == pytest.approx(5/3)
-    assert calcurator_instance.div(1, 3) == pytest.approx(1/3)
+    assert calculator_instance.div(5, 3) == pytest.approx(5/3)
+    assert calculator_instance.div(1, 3) == pytest.approx(1/3)
     with pytest.raises(ZeroDivisionError):
-        calcurator_instance.div(3,0)
+        calculator_instance.div(3,0)
         # ZeroDivisionError 가 발생하면 테스트 성공
         # 다른 결과가 나오거나 다른 에러 발생시 테스트 실패
 
-def test_multi(calcurator_instance):
+def test_multi(calculator_instance):
     
-    assert calcurator_instance.multi(3,2) == 6
-    assert calcurator_instance.multi(4, -4) == -16
-    assert calcurator_instance.multi(6, 0) == 0
+    assert calculator_instance.multi(3,2) == 6
+    assert calculator_instance.multi(4, -4) == -16
+    assert calculator_instance.multi(6, 0) == 0

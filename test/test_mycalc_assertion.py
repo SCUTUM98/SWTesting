3ddddcf95cfafdcd79d179b7e7d9484f7e2eab6d
BONGEUNGU <rnqhsrms98@gmail.com>
2025-08-29 15:10:00 +0900

    update test case

--- test/test_mycalc_assertion.py
+++ test/test_mycalc_assertion.py
@@ -1,4 +1,5 @@
 import pytest
+import warnings
 
 from apps.mycalc import add, sub, div
 
@@ -31,3 +32,14 @@ def test_type_error():
         div('4',3)
         # TypeError 가 발생하면 테스트 성공
         # 다른 결과가 나오거나 다른 에러 발생시 테스트 실패
+    # with pytest.raises(ValueError):
+    #     div('4',3)
+    #     # ValueError 가 발생하면 테스트 성공
+
+def function_that_warns():
+    warnings.warn("이 함수는 곧 제거될 예정입니다.", DeprecationWarning)
+    return True
+        
+def test_warning():
+    with pytest.warns(DeprecationWarning):
+        assert function_that_warns()

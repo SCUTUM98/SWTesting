07b8d781128f7a7dd7d6a56e6199fbf007c1d679
BONGEUNGU <rnqhsrms98@gmail.com>
2025-09-02 14:35:11 +0900

    fix playwright testing code

--- test/test_plw_login_pw.py
+++ test/test_plw_login_pw.py
@@ -1,14 +1,21 @@
 import pytest
 
+from web.plw_login_pw import LoginPage
 from playwright.sync_api import sync_playwright
 
 url = "https://the-internet.herokuapp.com/login"
+USERNAME = '#username'
+PW = '#password'
+BTN = 'button[type="submit"]'
+MSG = '#flash'
+
 LOGIN_CASES = [
     ('tomsmith', 'SuperSecretPassword!', 'You logged into a secure area!')
     , ('tomsmit', 'SuperSecretPassword!', 'Your username is invalid!')
     , ('tomsmith', 'SuperSecretPassword', 'Your password is invalid!')
     , ('', 'SuperSecretPassword!', 'Your username is invalid!')
-    , ('tomsmith', '', 'Your password is invalid!')]
+    , ('tomsmith', '', 'Your password is invalid!')
+    , ('', '', 'Your username is invalid!')]
 
 @pytest.fixture(scope='session')
 def browser():
@@ -26,12 +33,11 @@ def page(browser):
     
 @pytest.mark.parametrize('username, pw, expected', LOGIN_CASES)
 def test_login(page, username, pw, expected):
-    page.goto(url)
+    page = LoginPage(page)
+    page.open()
+    page.login(username, pw)
+    flash = page.flash_message()
     
-    page.fill('#username', username)
-    page.fill('#password', pw)
-    page.click('button[type="submit"]')
-    
-    flash_msg = page.locator('#flash').inner_text().strip()
-    assert expected in flash_msg
+    #flash_msg = page.locator(MSG).inner_text().strip()
+    assert expected in flash
     
